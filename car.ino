2d8541b9c56659190336a9c56aa860abc760586b
fcaster <52498759+fcaster@users.noreply.github.com>
2019-07-04 15:55:09 +0800

    Add files via upload

--- car.ino
+++ car.ino
@@ -1,6 +1,6 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(1,OUTPUT);
+  pinMode(7,OUTPUT);
   pinMode(2,OUTPUT);
   pinMode(3,OUTPUT);
   pinMode(4,OUTPUT);
@@ -25,7 +25,7 @@ void loop() {
   }
 }
 void forward(){
-  digitalWrite(1,HIGH);
+  digitalWrite(7,HIGH);
   digitalWrite(2,LOW);
   digitalWrite(3,HIGH);
   digitalWrite(4,LOW);
@@ -33,27 +33,27 @@ void forward(){
 
 void backward(){
   digitalWrite(2,HIGH);
-  digitalWrite(1,LOW);
+  digitalWrite(7,LOW);
   digitalWrite(4,HIGH);
   digitalWrite(3,LOW);
 }
 
 void turnleft(){
   digitalWrite(2,HIGH);
-  digitalWrite(1,LOW);
+  digitalWrite(7,LOW);
   digitalWrite(3,HIGH);
   digitalWrite(4,LOW);
 }
 
 void turnright(){
-  digitalWrite(1,HIGH);
+  digitalWrite(7,HIGH);
   digitalWrite(2,LOW);
   digitalWrite(4,HIGH);
   digitalWrite(3,LOW);
 }
 
 void zstop(){
-  digitalWrite(1,LOW);
+  digitalWrite(7,LOW);
   digitalWrite(2,LOW);
   digitalWrite(4,LOW);
   digitalWrite(3,LOW);
